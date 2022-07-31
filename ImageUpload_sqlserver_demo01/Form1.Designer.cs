c3b4ee7bb77f747eb0817a225de3043d258b278c
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-31 19:20:13 +0900

    プロジェクト ファイルを追加します。

---
+++ ImageUpload_sqlserver_demo01/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ImageUpload_sqlserver_demo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.database1DataSet = new ImageUpload_sqlserver_demo01.Database1DataSet();
+            this.picturesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.picturesTableAdapter = new ImageUpload_sqlserver_demo01.Database1DataSetTableAdapters.PicturesTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fileNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(395, 234);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textFileName
+            // 
+            this.textFileName.Location = new System.Drawing.Point(114, 273);
+            this.textFileName.Name = "textFileName";
+            this.textFileName.ReadOnly = true;
+            this.textFileName.Size = new System.Drawing.Size(474, 19);
+            this.textFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name :";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnUpload.Location = new System.Drawing.Point(594, 270);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(85, 23);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.fileNameDataGridViewTextBoxColumn,
+            this.imageDataGridViewImageColumn});
+            this.dataGridView.DataSource = this.picturesBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(16, 309);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 21;
+            this.dataGridView.Size = new System.Drawing.Size(663, 142);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // picturesBindingSource
+            // 
+            this.picturesBindingSource.DataMember = "Pictures";
+            this.picturesBindingSource.DataSource = this.database1DataSet;
+            // 
+            // picturesTableAdapter
+            // 
+            this.picturesTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fileNameDataGridViewTextBoxColumn
+            // 
+            this.fileNameDataGridViewTextBoxColumn.DataPropertyName = "FileName";
+            this.fileNameDataGridViewTextBoxColumn.HeaderText = "FileName";
+            this.fileNameDataGridViewTextBoxColumn.Name = "fileNameDataGridViewTextBoxColumn";
+            // 
+            // imageDataGridViewImageColumn
+            // 
+            this.imageDataGridViewImageColumn.DataPropertyName = "Image";
+            this.imageDataGridViewImageColumn.HeaderText = "Image";
+            this.imageDataGridViewImageColumn.Name = "imageDataGridViewImageColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 465);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textFileName);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Upload Image to Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource picturesBindingSource;
+        private Database1DataSetTableAdapters.PicturesTableAdapter picturesTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fileNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn imageDataGridViewImageColumn;
+    }
+}
+
---
+++ ImageUpload_sqlserver_demo01/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageUpload_sqlserver_demo01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: このコード行はデータを 'database1DataSet.Pictures' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+            //this.picturesTableAdapter.Fill(this.database1DataSet.Pictures);
+            LoadData();
+
+        }
+
+        public void Insert(string fileName, byte[] image)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[
+                "ImageUpload_sqlserver_demo01.Properties.Settings.Database1ConnectionString"].ConnectionString))
+            {
+                if(cn.State == ConnectionState.Closed)
+                    cn.Open();
+                using(SqlCommand cmd = SqlCommand("insert into pictures(filename, image) values(@filename, @image)"))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@filename", textFileName.Text);
+                    cmd.Parameters.AddWithValue("@image", image);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void LoadData()
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[
+                "ImageUpload_sqlserver_demo01.Properties.Settings.Database1ConnectionString"].ConnectionString))
+            {
+                if (cn.State == ConnectionState.Closed)
+                    cn.Open();
+                using(DataTable dt = new DataTable("pictures"))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter("select *from pictures", cn);
+                    adapter.Fill(dt);
+                    dataGridView.DataSource = dt;
+                }
+            }
+        }
+
+        byte[] ConvertImageToBytes(Image img)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        public Image ConvertByteArrayToImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter="Image files(*.jpg;*.jpeg)|*.jpg;*.jpeg", Multiselect=false })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //display image to pictureBox
+                    pictureBox.Image = Image.FromFile(ofd.FileName);
+                    //set path
+                    textFileName.Text = ofd.FileName;
+                    //insert data to localdatabase, then reload data
+                    Insert(textFileName.Text, ConvertImageToBytes(pictureBox.Image));
+                    LoadData();
+                }
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable dt = dataGridView.DataSource as DataTable;
+            if(dt != null)
+            {
+                DataRow row = dt.Rows[e.RowIndex];
+                pictureBox.Image = ConvertByteArrayToImage((byte[])row["Image"]);
+            }
+        }
+    }
+}
